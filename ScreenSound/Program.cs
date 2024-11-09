74e458668265deffc8fe918950f7e7364411ae8a
Victor Hugo <133723528+Victor-SB@users.noreply.github.com>
2024-11-09 17:56:47 -0300

    Exibindo musicas de um album

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,15 +1,15 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Roxane";
-musica1.Artista = "The Police";
-musica1.Duracao = 273;
-musica1.Disponivel = true;
-Console.WriteLine(musica1.DescricaoResumida);
+﻿Album albumDoQuenn = new Album();
+albumDoQuenn.Nome = "A night at the opera";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Love of my life";
+musica1.Duracao = 213;
 
 Musica musica2 = new Musica();
-musica2.Nome = "Vertigo";
-musica2.Artista = "U2";
-musica2.Duracao = 367;
-musica2.Disponivel = false;
+musica2.Nome = "Bohemian Rhapsody";
+musica2.Duracao = 364;
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
+albumDoQuenn.AdicionarMusica(musica1);
+albumDoQuenn.AdicionarMusica(musica2);
+
+albumDoQuenn.ExibirMusicasDoAlbum();
