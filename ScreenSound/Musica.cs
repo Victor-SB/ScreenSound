34c5a25b827eee8b31b18c1bbe19da87121df38a
Victor Hugo <133723528+Victor-SB@users.noreply.github.com>
2024-11-10 16:25:07 -0300

    Desenvolvendo a classe banda e adicionando construtores

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -1,16 +1,24 @@
 ﻿class Musica
 {
-    public string Nome { get; set; }
-    public string Artista { get; set; }
+
+    //Contrtutor com parâmetro
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+    public string Nome { get; }
+    public Banda Artista { get; }
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
+    public Genero Genero { get; set; }
     public string DescricaoResumida => 
         $"A música {Nome} pertence á banda {Artista}"; //Propriedade somente leitura
    
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {Nome}");
-        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
         Console.WriteLine($"Duração: {Duracao}");
         if (Disponivel)
         {
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,15 +1,25 @@
-﻿Album albumDoQuenn = new Album();
-albumDoQuenn.Nome = "A night at the opera";
+﻿//Método que chama a banda
+Banda queen = new Banda("Queen");
 
-Musica musica1 = new Musica();
-musica1.Nome = "Love of my life";
-musica1.Duracao = 213;
+Album albumDoQuenn = new Album("A night at the opera");
 
-Musica musica2 = new Musica();
-musica2.Nome = "Bohemian Rhapsody";
-musica2.Duracao = 364;
+Musica musica1 = new Musica(queen, "Love of my life")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
 
 albumDoQuenn.AdicionarMusica(musica1);
 albumDoQuenn.AdicionarMusica(musica2);
+queen.AdicionarAlbum(albumDoQuenn);
 
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
 albumDoQuenn.ExibirMusicasDoAlbum();
+queen.ExibirDiscografia();
