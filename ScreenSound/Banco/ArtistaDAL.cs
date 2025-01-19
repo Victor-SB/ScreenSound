88f0865c15398cfdb2498c8105c2dae0a09b12a0
Victor Hugo <133723528+Victor-SB@users.noreply.github.com>
2025-01-19 14:58:08 -0300

    implementando métodos genéricos

--- ScreenSound/Banco/ArtistaDAL.cs
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -12,37 +12,7 @@ namespace ScreenSound.Banco
 
     internal class ArtistaDAL: DAL<Artista>
     {
-        private readonly ScreenSoundContext context;
-
-        public ArtistaDAL(ScreenSoundContext context)
-        {
-            this.context = context;
-        }
-
-        public override IEnumerable<Artista> Listar()
-        {
-            return context.Artistas.ToList();
-        }
-
-        //Método adicionar novos artistas
-        public override void Adicionar(Artista artista)
-        {
-            context.Artistas.Add(artista);
-            context.SaveChanges();
-        }
-
-        //Método atualizar artistas
-        public  override void Atualizar(Artista artista)
-        {
-            context.Artistas.Update(artista);
-            context.SaveChanges();
-        }
-        public override void Deletar(Artista artista)
-        {
-            context.Artistas.Remove(artista);
-            context.SaveChanges();
-        }
-
+        public ArtistaDAL(ScreenSoundContext context): base(context) { }
         public Artista? RecuperarPeloNome(string nome)
         {
             return context.Artistas.FirstOrDefault(a => a.Nome.Equals(nome));
