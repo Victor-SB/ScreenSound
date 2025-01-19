600d83ed4a16c5a99d0553ac0be9468f7ffeb2ff
Victor Hugo <133723528+Victor-SB@users.noreply.github.com>
2025-01-19 17:23:35 -0300

    inserindo dados e atualizando DataBase

--- ScreenSound/Modelos/Program.cs
+++ ScreenSound/Modelos/Program.cs
@@ -8,7 +8,7 @@ var context = new ScreenSoundContext();
 //var musicaDAL = new ScreenSoundContext();
 
 {
-    var musicaDAL = new MusicaDAL(context);
+    /*var musicaDAL = new MusicaDAL(context);
 
     // Adicionar uma nova música
     var novaMusica = new Musica { Nome = "Nome da Música" };
@@ -27,18 +27,18 @@ var context = new ScreenSoundContext();
 
     // Deletar uma música
     musicaDAL.Deletar(novaMusica);
-}
+}*/
 
-Dictionary<int, Menu> opcoes = new();
-opcoes.Add(1, new MenuRegistrarArtista());
-opcoes.Add(2, new MenuRegistrarMusica());
-opcoes.Add(3, new MenuMostrarArtistas());
-opcoes.Add(4, new MenuMostrarMusicas());
-opcoes.Add(-1, new MenuSair());
+    Dictionary<int, Menu> opcoes = new();
+    opcoes.Add(1, new MenuRegistrarArtista());
+    opcoes.Add(2, new MenuRegistrarMusica());
+    opcoes.Add(3, new MenuMostrarArtistas());
+    opcoes.Add(4, new MenuMostrarMusicas());
+    opcoes.Add(-1, new MenuSair());
 
-void ExibirLogo()
-{
-    Console.WriteLine(@"
+    void ExibirLogo()
+    {
+        Console.WriteLine(@"
 
 ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
 ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
@@ -47,32 +47,33 @@ void ExibirLogo()
 ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
 ");
-    Console.WriteLine("Boas vindas ao Screen Sound 3.0!");
-}
-
-void ExibirOpcoesDoMenu()
-{
-    ExibirLogo();
-    Console.WriteLine("\nDigite 1 para registrar um artista");
-    Console.WriteLine("Digite 2 para registrar a música de um artista");
-    Console.WriteLine("Digite 3 para mostrar todos os artistas");
-    Console.WriteLine("Digite 4 para exibir todas as músicas de um artista");
-    Console.WriteLine("Digite -1 para sair");
-
-    Console.Write("\nDigite a sua opção: ");
-    string opcaoEscolhida = Console.ReadLine()!;
-    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
-
-    if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
-    {
-        Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
-        menuASerExibido.Executar(artistaDAL);
-        if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+        Console.WriteLine("Boas vindas ao Screen Sound 3.0!");
     }
-    else
-    {
-        Console.WriteLine("Opção inválida");
-    }
-}
 
-ExibirOpcoesDoMenu();
+    void ExibirOpcoesDoMenu()
+    {
+        ExibirLogo();
+        Console.WriteLine("\nDigite 1 para registrar um artista");
+        Console.WriteLine("Digite 2 para registrar a música de um artista");
+        Console.WriteLine("Digite 3 para mostrar todos os artistas");
+        Console.WriteLine("Digite 4 para exibir todas as músicas de um artista");
+        Console.WriteLine("Digite -1 para sair");
+
+        Console.Write("\nDigite a sua opção: ");
+        string opcaoEscolhida = Console.ReadLine()!;
+        int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+        if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
+        {
+            Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
+            //menuASerExibido.Executar(artistaDAL);
+            if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+        }
+        else
+        {
+            Console.WriteLine("Opção inválida");
+        }
+    }
+
+    ExibirOpcoesDoMenu();
+}
