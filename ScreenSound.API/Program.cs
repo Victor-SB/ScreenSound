e5e299b0bb0d573e83ee9deb95c1c0196644154a
Victor Hugo <victor.hugo.barbosa@hotmail.com>
2025-04-16 15:12:53 -0300

    feat: adiciona funcionalidade para excluir artista na API

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -18,7 +18,7 @@ app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) =>
 
 app.MapGet("/Artistas/{nome}", ([FromServices] DAL<Artista> dal, string nome) =>
 {
-    var artista = dal.RecuperarPor(a=>a.Nome.ToUpper().Equals(nome.ToUpper()));
+    var artista = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
     if (artista is null)
     {
         return Results.NotFound();
@@ -26,10 +26,21 @@ app.MapGet("/Artistas/{nome}", ([FromServices] DAL<Artista> dal, string nome) =>
     return Results.Ok(artista);
 });
 
-app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody]Artista artista) => 
+app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista artista) =>
 {
     dal.Adicionar(artista);
     return Results.Ok();
+});
+
+app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) =>
+{
+    var artista = dal.RecuperarPor(a => a.Id == id);
+    if (artista is null)
+    {
+        return Results.NotFound();
+    }
+    dal.Deletar(artista);
+    return Results.NoContent();
 });
 
 app.Run();
