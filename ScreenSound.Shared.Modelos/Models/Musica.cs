b3ace9159f8856ac783c4b0301624a4d4b2f0ee3
Victor Hugo <victor.hugo.barbosa@hotmail.com>
2025-04-23 16:47:51 -0300

    refactor: renomeia pasta de Modelos para Models para padronização

---
+++ ScreenSound.Shared.Modelos/Models/Musica.cs
@@ -0,0 +1,29 @@
+﻿namespace ScreenSound.Modelos;
+public class Musica
+{
+
+    //Contrtutor com parâmetro
+    public Musica(string nome, int anoLancamento)
+    {
+        Nome = nome;
+        AnoLancamento = anoLancamento;
+    }
+
+    public string Nome { get; set; }
+    public int Id { get; set; }
+    public int Duracao { get; internal set; }
+    public int AnoLancamento { get; set; }
+    public virtual Artista? Artista { get; set; }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+
+    }
+
+    public override string ToString()
+    {
+        return @$"Id: {Id}
+        Nome: {Nome}";
+    }
+}
---
+++ ScreenSound.Shared.Modelos/Models/Podcast.cs
@@ -0,0 +1,35 @@
+﻿namespace ScreenSound.Modelos;
+class Podcast
+{
+    private List<Episodio> episodios = new();
+    public Podcast( string host, string nome)
+    {
+        Host = host;
+        Nome = nome;
+    }
+    public string Host { get; }
+    public string Nome { get; }
+    public int TotalEpsodios => episodios.Count;
+
+    public void AdicionarEpsodio(Episodio episodio)
+    {
+        episodios.Add(episodio);
+    }
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Nome do Podcast: {Nome} Apresentado por: {Host}\n");
+
+        // Exibe os detalhes dos epsódios
+        foreach (Episodio episodio in episodios.OrderBy(e => e.Ordem))
+        {
+            Console.WriteLine($"Epsódio {episodio.Ordem}:{episodio.Titulo}");
+            Console.WriteLine($"Duração: {episodio.Duracao}");
+            Console.WriteLine($"Resumo: {episodio.Resumo}\n");
+
+        }
+
+        //Exibe o total de episódios
+        Console.WriteLine($"Este Podcast possui o total de episódios: {episodios.Count}");
+
+    }
+}
