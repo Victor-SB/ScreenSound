4da16ac0e84a8a90178d5932b18672a76f3061ef
Victor Hugo <victor.hugo.barbosa@hotmail.com>
2025-05-16 10:46:10 -0300

    Feat: Aplicar tema personalizado ao projeto

---
+++ ScreenSound.Web/Layout/ScreensoundPallete.cs
@@ -0,0 +1,18 @@
+﻿using MudBlazor;
+using MudBlazor.Utilities;
+using System.Runtime.InteropServices;
+
+namespace ScreenSound.Web.Layout;
+
+public sealed class ScreensoundPallete : PaletteDark
+{ 
+    private ScreensoundPallete()
+{
+    Primary = new MudColor("#9966FF");
+    Secondary = new MudColor("#F6AD31");
+    Tertiary = new MudColor("#8AE491");
+}
+
+public static ScreensoundPallete CreatePallete => new();
+
+}
