ec0661662f0dfefceb0890d450312c8614aeab4f
Victor Hugo <victor.hugo.barbosa@hotmail.com>
2025-05-17 20:11:25 -0300

    feat: adiciona função de exclusão de artista

--- ScreenSound.Web/Services/ArtistaAPI.cs
+++ ScreenSound.Web/Services/ArtistaAPI.cs
@@ -23,4 +23,14 @@ public class ArtistaAPI
     {
         await _httpClient.PostAsJsonAsync("artistas", artista);
     }
+
+    public async Task DeleteArtistaAsync(int id)
+    {
+        await _httpClient.DeleteAsync($"artistas/{id}");
+    }
+
+    public async Task<ArtistaResponse?> GetArtistaPorNomeAsync(string nome)
+    {
+        return await _httpClient.GetFromJsonAsync<ArtistaResponse>($"artistas/{nome}");
+    }
 }
